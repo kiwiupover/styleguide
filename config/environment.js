5dc8186ddeeab3ce529de51d9a338cb94d4dbaf6
Dave Laird <kiwiupover@gmail.com>
2017-02-03 08:21:22 -0800

    style guide started

---
+++ config/environment.js
@@ -0,0 +1,11 @@
+/*jshint node:true*/
+'use strict';
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'styleguide',
+    environment: environment
+  }
+
+  return ENV;
+};
