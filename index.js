6873e63044fd80e9b7a8dbc447b909077e47627f
Dave Laird <kiwiupover@gmail.com>
2017-03-30 12:35:11 -0700

    Adding deps to the styleguide index.js

--- index.js
+++ index.js
@@ -8,5 +8,41 @@ const EngineAddon = require('ember-engines/lib/engine-addon');
 
 module.exports = EngineAddon.extend({
   name: 'styleguide',
-  lazyLoading: true
+  lazyLoading: true,
+
+  included: function(app) {
+    this._super.included(app);
+    var target = app || parentAddon;
+    if (target.import) {
+
+      target.import('vendor/highlight.pack.js', {
+        exports: {
+          'highlight.js': [
+            'default',
+            'highlight',
+            'highlightAuto',
+            'highlightBlock'
+          ]
+        }
+      });
+
+      target.import('vendor/markdown-it.min.js');
+
+      target.import('vendor/shims/highlight.js');
+      target.import('vendor/shims/markdown-it.js');
+    }
+
+  },
+
+  treeForVendor(vendorTree) {
+    let markdownItTree = new Funnel(path.join(this.project.root, 'node_modules', 'markdown-it', 'dist'), {
+      files: ['markdown-it.min.js'],
+    });
+
+    let highlightTree = new Funnel(path.join(this.project.root, 'node_modules', 'highlightjs'), {
+      files: ['highlight.pack.js'],
+    });
+
+    return new MergeTrees([vendorTree, markdownItTree, highlightTree]);
+  }
 });
