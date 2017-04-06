76623b38f9c9ff2fdd19b4a964aeb6e8e95bfd6c
Dave Laird <kiwiupover@gmail.com>
2017-04-05 17:17:19 -0700

    Upgrade ember

--- index.js
+++ index.js
@@ -5,42 +5,46 @@ const Funnel = require('broccoli-funnel');
 
 const MergeTrees = require('ember-cli/lib/broccoli/merge-trees');
 const EngineAddon = require('ember-engines/lib/engine-addon');
+var debug = require('broccoli-stew').debug;
 
 module.exports = EngineAddon.extend({
   name: 'styleguide',
   lazyLoading: true,
 
-  included: function(app) {
-    this._super.included(app);
-
-    this.import('vendor/highlight.pack.js', {
-      exports: {
-        'highlight.js': [
-          'default',
-          'highlight',
-          'highlightAuto',
-          'highlightBlock'
-        ]
-      }
-    });
-
-    this.import('vendor/markdown-it.min.js');
-
-    this.import('vendor/shims/highlight.js');
-    this.import('vendor/shims/markdown-it.js');
-  },
-
-  treeForVendor() {
-    var tree = this._super.treeForVendor.apply(this, arguments);
-
-    let markdownItTree = new Funnel(path.join(this.project.root, 'node_modules', 'markdown-it', 'dist'), {
-      files: ['markdown-it.min.js'],
-    });
-
-    let highlightTree = new Funnel(path.join(this.project.root, 'node_modules', 'highlightjs'), {
-      files: ['highlight.pack.js'],
-    });
-
-    return new MergeTrees([tree, markdownItTree, highlightTree]);
-  }
+  // included: function(app) {
+  //   this._super.included(app);
+  //
+  //   this.import('vendor/highlight.pack.js', {
+  //     exports: {
+  //       'highlight.js': [
+  //         'default',
+  //         'highlight',
+  //         'highlightAuto',
+  //         'highlightBlock'
+  //       ]
+  //     }
+  //   });
+  //
+  //   this.import('vendor/markdown-it.min.js');
+  //
+  //   this.import('vendor/shims/highlight.js');
+  //   this.import('vendor/shims/markdown-it.js');
+  // },
+  //
+  // treeForVendor() {
+  //   var tree = this._super.treeForVendor.apply(this, arguments);
+  //
+  //   let markdownItTree = new Funnel(path.join(this.project.root, 'node_modules', 'markdown-it', 'dist'), {
+  //     files: ['markdown-it.min.js'],
+  //   });
+  //
+  //   let highlightTree = new Funnel(path.join(this.project.root, 'node_modules', 'highlightjs'), {
+  //     files: ['highlight.pack.js'],
+  //   });
+  //
+  //   return new MergeTrees([tree, markdownItTree, highlightTree]);
+  //   let loggedApp = debug(vendorTreeDebug, { name: 'addonTreeDebug' });
+  //
+  //   return loggedApp
+  // }
 });
